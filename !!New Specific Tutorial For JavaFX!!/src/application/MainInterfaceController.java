214b606141512163975be34b7f3da1d0afc44810
GOXR3PLUS <crAlexcomp@gmail.com>
2017-05-15 16:02:20 +0300

    Update 15/05/2017
    Added new Examples for Java FX!

---
+++ !!New Specific Tutorial For JavaFX!!/src/application/MainInterfaceController.java
@@ -0,0 +1,90 @@
+package application;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * The main interface of the application
+ * 
+ * @author GOXR3PLUS
+ *
+ */
+public class MainInterfaceController extends BorderPane {
+	
+	@FXML
+	private Button start;
+	
+	@FXML
+	private Button pause;
+	
+	@FXML
+	private Button resume;
+	
+	@FXML
+	private Label statusLabel;
+	
+	@FXML
+	private TextArea infoArea;
+	
+	// -----------------------------------------
+	
+	private SpeechRecognizer speechRecognition = new SpeechRecognizer();
+	
+	/**
+	 * Constructor
+	 */
+	public MainInterfaceController() {
+		
+		// FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainInterfaceController.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+		
+		try {
+			loader.load();
+		} catch (IOException ex) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, " FXML can't be loaded!", ex);
+		}
+		
+	}
+	
+	/**
+	 * Called as soon as .fxml is initialised
+	 */
+	@FXML
+	private void initialize() {
+		
+		// start
+		start.disableProperty().bind(speechRecognition.speechRecognizerThreadRunningProperty());
+		start.setOnAction(a -> {
+			statusLabel.setText("Status : [Running]");
+			infoArea.appendText("Starting Speech Recognizer\n");
+			speechRecognition.startSpeechRecognition();
+		});
+		
+		// stop
+		pause.disableProperty().bind(speechRecognition.ignoreSpeechRecognitionResultsProperty().or(start.disabledProperty().not()));
+		pause.setOnAction(a -> {
+			statusLabel.setText("Status : [Paused]");
+			infoArea.appendText("Pausing Speech Recognizer\n");
+			speechRecognition.ignoreSpeechRecognitionResults();
+		});
+		
+		// restart
+		resume.disableProperty().bind(speechRecognition.ignoreSpeechRecognitionResultsProperty().not());
+		resume.setOnAction(a -> {
+			statusLabel.setText("Status : [Running]");
+			infoArea.appendText("Resuming Speech Recognizer\n");
+			speechRecognition.stopIgnoreSpeechRecognitionResults();
+		});
+		
+	}
+}
---
+++ !!New Specific Tutorial For JavaFX!!/src/application/SpeechRecognizer.java
@@ -0,0 +1,256 @@
+package application;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Port;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.result.WordResult;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public class SpeechRecognizer {
+	
+	// Necessary
+	private LiveSpeechRecognizer recognizer;
+	
+	// Logger
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	/**
+	 * This String contains the Result that is coming back from SpeechRecognizer
+	 */
+	private String speechRecognitionResult;
+	
+	//-----------------Lock Variables-----------------------------
+	
+	/**
+	 * This variable is used to ignore the results of speech recognition
+	 * cause actually it can't be stopped...
+	 * 
+	 * <br>
+	 * Check this link for more information:
+	 * <a href=
+	 * "https://sourceforge.net/p/cmusphinx/discussion/sphinx4/thread/3875fc39/">https://sourceforge.net/p/cmusphinx/discussion/sphinx4/thread/3875fc39/</a>
+	 */
+	private SimpleBooleanProperty ignoreSpeechRecognitionResults = new SimpleBooleanProperty(false);
+	
+	/**
+	 * Checks if the speech recognise is already running
+	 */
+	private SimpleBooleanProperty speechRecognizerThreadRunning = new SimpleBooleanProperty(false);
+	
+	/**
+	 * Checks if the resources Thread is already running
+	 */
+	private boolean resourcesThreadRunning;
+	
+	//---
+	
+	/**
+	 * This executor service is used in order the playerState events to be
+	 * executed in an order
+	 */
+	private ExecutorService eventsExecutorService = Executors.newFixedThreadPool(2);
+	
+	//------------------------------------------------------------------------------------
+	
+	/**
+	 * Constructor
+	 */
+	public SpeechRecognizer() {
+		
+		// Loading Message
+		logger.log(Level.INFO, "Loading Speech Recognizer...\n");
+		
+		// Configuration
+		Configuration configuration = new Configuration();
+		
+		// Load model from the jar
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		
+		// if you want to use LanguageModelPath disable the 3 lines after which
+		// are setting a custom grammar->
+		
+		// configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin")
+		
+		// Grammar
+		configuration.setGrammarPath("resource:/grammars");
+		configuration.setGrammarName("grammar");
+		configuration.setUseGrammar(true);
+		
+		try {
+			recognizer = new LiveSpeechRecognizer(configuration);
+		} catch (IOException ex) {
+			logger.log(Level.SEVERE, null, ex);
+		}
+		
+		// Start recognition process pruning previously cached data.
+		// recognizer.startRecognition(true);
+		
+		//Start the Threads
+		startResourcesThread();
+		//startSpeechRecognition();
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	/**
+	 * Starts the Speech Recognition Thread
+	 */
+	public synchronized void startSpeechRecognition() {
+		
+		//Check lock
+		if (speechRecognizerThreadRunning.get())
+			logger.log(Level.INFO, "Speech Recognition Thread already running...\n");
+		else
+			//Submit to ExecutorService
+			eventsExecutorService.submit(() -> {
+				
+				//locks
+				Platform.runLater(() -> {
+					speechRecognizerThreadRunning.set(true);
+					ignoreSpeechRecognitionResults.set(false);
+				});
+				
+				//Start Recognition
+				recognizer.startRecognition(true);
+				
+				//Information			
+				logger.log(Level.INFO, "You can start to speak...\n");
+				
+				try {
+					while (speechRecognizerThreadRunning.get()) {
+						/*
+						 * This method will return when the end of speech is
+						 * reached. Note that the end pointer will determine the
+						 * end
+						 * of speech.
+						 */
+						SpeechResult speechResult = recognizer.getResult();
+						
+						//Check if we ignore the speech recognition results
+						if (!ignoreSpeechRecognitionResults.get()) {
+							
+							//Check the result
+							if (speechResult == null)
+								logger.log(Level.INFO, "I can't understand what you said.\n");
+							else {
+								
+								//Get the hypothesis
+								speechRecognitionResult = speechResult.getHypothesis();
+								
+								//You said?
+								System.out.println("You said: [" + speechRecognitionResult + "]\n");
+								
+								//Call the appropriate method 
+								makeDecision(speechRecognitionResult, speechResult.getWords());
+								
+							}
+						} else
+							logger.log(Level.INFO, "Ingoring Speech Recognition Results...");
+						
+					}
+				} catch (Exception ex) {
+					logger.log(Level.WARNING, null, ex);
+					Platform.runLater(() -> speechRecognizerThreadRunning.set(false));
+				}
+				
+				logger.log(Level.INFO, "SpeechThread has exited...");
+				
+			});
+	}
+	
+	/**
+	 * Stops ignoring the results of SpeechRecognition
+	 */
+	public synchronized void stopIgnoreSpeechRecognitionResults() {
+		
+		//Stop ignoring speech recognition results
+		Platform.runLater(() -> ignoreSpeechRecognitionResults.set(false));
+	}
+	
+	/**
+	 * Ignores the results of SpeechRecognition
+	 */
+	public synchronized void ignoreSpeechRecognitionResults() {
+		
+		//Instead of stopping the speech recognition we are ignoring it's results
+		Platform.runLater(() -> ignoreSpeechRecognitionResults.set(true));
+		
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	/**
+	 * Starting a Thread that checks if the resources needed to the
+	 * SpeechRecognition library are available
+	 */
+	public void startResourcesThread() {
+		
+		//Check lock
+		if (resourcesThreadRunning)
+			logger.log(Level.INFO, "Resources Thread already running...\n");
+		else
+			//Submit to ExecutorService
+			eventsExecutorService.submit(() -> {
+				try {
+					
+					//Lock
+					resourcesThreadRunning = true;
+					
+					// Detect if the microphone is available
+					while (true) {
+						
+						//Is the Microphone Available
+						if (!AudioSystem.isLineSupported(Port.Info.MICROPHONE))
+							logger.log(Level.INFO, "Microphone is not available.\n");
+						
+						// Sleep some period
+						Thread.sleep(350);
+					}
+					
+				} catch (InterruptedException ex) {
+					logger.log(Level.WARNING, null, ex);
+					resourcesThreadRunning = false;
+				}
+			});
+	}
+	
+	/**
+	 * Takes a decision based on the given result
+	 * 
+	 * @param speechWords
+	 */
+	public void makeDecision(String speech , List<WordResult> speechWords) {
+		
+		// Split the sentence
+		// System.out.println("SpeechWords: " +
+		// Arrays.toString(speechWords.toArray()))
+		// if (!speech.contains("hey"))
+		// return;
+		// else
+		// speech = speech.replace("hey", "");
+		
+		System.out.println(speech);
+		
+	}
+	
+	public SimpleBooleanProperty ignoreSpeechRecognitionResultsProperty() {
+		return ignoreSpeechRecognitionResults;
+	}
+	
+	public SimpleBooleanProperty speechRecognizerThreadRunningProperty() {
+		return speechRecognizerThreadRunning;
+	}
+	
+}
